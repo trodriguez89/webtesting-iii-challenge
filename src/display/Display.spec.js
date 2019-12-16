11ed75349ba225ff10cfb173b67d77c28ff1279f
trodriguez89 <trodriguez89@gmail.com>
2019-12-16 12:42:56 -0800

    display snapshot created

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,9 @@
 // Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Display from "./Display";
+
+test("Display matches snapshot", () => {
+    const tree = render (<Display />)
+    expect(tree.baseElement).toMatchSnapshot();
+}) 

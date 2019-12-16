332295c5b51a8c40765eef9b38f878837f32bb43
trodriguez89 <trodriguez89@gmail.com>
2019-12-16 13:31:29 -0800

    controls, display tests working

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -5,10 +5,10 @@ const Controls = props => {
 
   return (
     <div className="controls panel">
-      <button disabled={!closed} onClick={toggleLocked} className="toggle-btn">
+      <button data-testid="locked" disabled={!closed} onClick={toggleLocked} className="toggle-btn">
         {locked ? 'Unlock Gate' : 'Lock Gate'}
       </button>
-      <button disabled={locked} onClick={toggleClosed} className="toggle-btn">
+      <button data-testid="closed" disabled={locked} onClick={toggleClosed} className="toggle-btn">
         {closed ? 'Open Gate' : 'Close Gate'}
       </button>
     </div>
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -6,4 +6,13 @@ import Display from "./Display";
 test("Display matches snapshot", () => {
     const tree = render (<Display />)
     expect(tree.baseElement).toMatchSnapshot();
-}) 
+});
+
+test("display  green when unlocked or open", () => {
+    const display = render(<Display closed={false} locked={false}/>);
+    const lockStatus = display.getByText(/unlocked/i);
+    const openStatus = display.getByText(/open/i);
+
+    expect(lockStatus.className).toBe("led green-led");
+    expect(openStatus.className).toBe("led green-led");
+});

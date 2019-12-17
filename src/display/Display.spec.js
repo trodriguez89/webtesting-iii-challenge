f5c6a57bdb691296fd998b0de9a6d16c5e0f25e1
trodriguez89 <trodriguez89@gmail.com>
2019-12-17 12:03:17 -0800

    mvp finished

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -8,11 +8,20 @@ test("Display matches snapshot", () => {
     expect(tree.baseElement).toMatchSnapshot();
 });
 
-test("display  green when unlocked or open", () => {
+test("display green when unlocked or open", () => {
     const display = render(<Display closed={false} locked={false}/>);
     const lockStatus = display.getByText(/unlocked/i);
     const openStatus = display.getByText(/open/i);
 
     expect(lockStatus.className).toBe("led green-led");
     expect(openStatus.className).toBe("led green-led");
+});
+
+test("display red when locked or closed", () => {
+    const display2 = render(<Display closed={true} locked={true}/>)
+    const lockStatus2 = display2.getByText(/locked/i);
+    const openStatus2 = display2.getByText(/close/i);
+
+    expect(lockStatus2.className).toBe("led red-led");
+    expect(openStatus2.className).toBe("led red-led");
 });
